c47737c5469a011263becd0917cc251b575eb2ec
Jason-Leiby <jason.leiby@ibm.com>
2022-06-03 13:55:34 -0600

    update endpoints for files

--- watson.sh
+++ watson.sh
@@ -21,13 +21,13 @@ while [ ! -f /root/watsondiscoveryInst.txt ]; do
     sleep 1
 done
 
-curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daidemos.com/log
-wget -O /root/secData.txt "https://daidemos.com/secData?company=$(< /root/companytitle.txt)"
+curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daitest.leibcorp.com/log
+wget -O /root/secData.txt "https://daitest.leibcorp.com/secData?company=$(< /root/companytitle.txt)"
 
 dvar=`cat /root/secData.txt | grep -c 'none'`
 if [ $dvar -gt 0 ]
 then
-wget -O /root/10k.tgz https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/10k.tgz
+wget -O /root/10k.tgz https://raw.githubusercontent.com/ch0pper1/demo-scripts/main/10k.tgz
 tar -xvzf /root/10k.tgz -C /root
 else
 mkdir /root/10k
@@ -35,15 +35,15 @@ wget -O /root/da/sec.js https://raw.githubusercontent.com/garrettrowe/watsonAuto
 (node /root/da/sec.js "$(< /root/secData.txt)"  > /var/log/secData.log 2>&1 ) &
 fi
 
-wget -O /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/flowsV2.json
-wget -O /root/discovery.tgz https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/discoV2.tgz
+wget -O /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json https://raw.githubusercontent.com/ch0pper1/demo-scripts/main/flowsV2.json
+wget -O /root/discovery.tgz https://raw.githubusercontent.com/ch0pper1/demo-scripts/main/discoV2.tgz
 wget -O /root/finance_dte_model.zip https://ibm.box.com/shared/static/h7sca2gyrz0az58vicqlqijh1mvtl71u.zip
 
-wget -O /root/upsell.zip https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/upsell.zip
+wget -O /root/upsell.zip https://raw.githubusercontent.com/ch0pper1/demo-scripts/main/upsell.zip
 tar -xvzf /root/discovery.tgz -C /root
 npm --prefix /root/discoveryService install /root/discoveryService
 
-curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Services" -X POST https://daidemos.com/log
+curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Services" -X POST https://daitest.leibcorp.com/log
 systemctl enable nodered.service
 systemctl start nodered.service
-node /root/da/pmw.js "https://$(< /root/instnum.txt)-target.daidemos.com/" "$(< /root/companyurl.txt)" &
+node /root/da/pmw.js "https://$(< /root/instnum.txt)-target.daitest.leibcorp.com/" "$(< /root/companyurl.txt)" &
