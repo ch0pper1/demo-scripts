d3d86c74190411e3eceb64546cd8ce06bbfe6739
Jason-Leiby <jason.leiby@ibm.com>
2022-07-21 12:50:58 -0600

    new action file and downgrade node

--- watson.sh
+++ watson.sh
@@ -34,6 +34,9 @@ mkdir /root/10k
 wget -O /root/da/sec.js https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/sec.js
 (node /root/da/sec.js "$(< /root/secData.txt)"  > /var/log/secData.log 2>&1 ) &
 fi
+# downgrade node and npm
+npm install --location=global npm@6
+npm install -g node@14
 
 wget -O /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/flowsV3.json
 wget -O /root/discovery.tgz https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/discoV2.tgz
