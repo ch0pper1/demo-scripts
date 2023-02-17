0303e6f05ea6e70fa501c49cb4f56bbf5653351d
Jason-Leiby <jason.leiby@ibm.com>
2023-02-17 07:41:07 -0700

    updated base and watson scripts for base image

--- watson.sh
+++ watson.sh
@@ -2,6 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive
 export HOME=/root
 export USER=root
+
+# Set home server URL
+url="daitest.leibcorp.com"
+
 while [ ! -f /root/instnum.txt ]; do
     sleep 1
 done
@@ -21,31 +25,27 @@ while [ ! -f /root/watsondiscoveryInst.txt ]; do
     sleep 1
 done
 
-curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daitest.leibcorp.com/log
-wget -O /root/secData.txt "https://daitest.leibcorp.com/secData?company=$(< /root/companytitle.txt)"
+curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://$url/log
+wget -O /root/secData.txt "https://$url/secData?company=$(< /root/companytitle.txt)"
 
 dvar=`cat /root/secData.txt | grep -c 'none'`
 if [ $dvar -gt 0 ]
 then
-wget -O /root/10k.tgz https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/10k.tgz
 tar -xvzf /root/10k.tgz -C /root
 else
 mkdir /root/10k
-wget -O /root/da/sec.js https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/sec.js
 (node /root/da/sec.js "$(< /root/secData.txt)"  > /var/log/secData.log 2>&1 ) &
 fi
 # downgrade node and npm
 npm install --location=global npm@6
 
-wget -O /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/flowsV3.json
-wget -O /root/discovery.tgz https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/discoV2.tgz
-wget -O /root/finance_dte_model.zip https://ibm.box.com/shared/static/h7sca2gyrz0az58vicqlqijh1mvtl71u.zip
-
-wget -O /root/upsell.zip https://raw.githubusercontent.com/ch0pper1/demo-scripts/master/upsell.zip
+# need to move flows to proper folder
+mv /root/flows.json /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json
 tar -xvzf /root/discovery.tgz -C /root
 npm --prefix /root/discoveryService install /root/discoveryService
 
-curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Services" -X POST https://daitest.leibcorp.com/log
+curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Services" -X POST https://$url/log
 systemctl enable nodered.service
 systemctl start nodered.service
-node /root/da/pmw.js "https://$(< /root/instnum.txt)-target.daitest.leibcorp.com/" "$(< /root/companyurl.txt)" &
+node /root/da/pmw.js "https://$(< /root/instnum.txt)-target.$url/" "$(< /root/companyurl.txt)" &
+
